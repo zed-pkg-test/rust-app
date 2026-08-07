35128fc5b2cbf43dd033672312deb72435f79cf4
Alexander Mills <alex@oresoftware.com>
2026-08-07 14:08:00 -0500

    test: use a valid Unicode identifier continuation

--- tests/cargo_zed_interop.rs
+++ tests/cargo_zed_interop.rs
@@ -13,5 +13,5 @@ fn target_and_build_dependency_feature_sets_remain_separate() {
 #[test]
 fn zed_materialized_crate_keeps_its_cargo_dependency_graph() {
     assert_eq!(rust_lib::format_count(u64::MAX), "18446744073709551615");
-    assert!(unicode_ident::is_xid_continue('₀'));
+    assert!(unicode_ident::is_xid_continue('δ'));
 }
