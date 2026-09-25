5342a2dbe710b2de8e7ca230bfa13908a3c122c8
Mr. Denman Mills <alexmillsmail@gmail.com>
2026-09-25 13:59:14 -0400

    proof: add fixtures/phoenix-discovery/lib/bmscl_phoenix_fixture_web/controllers.ex

---
+++ phoenix-e2e/fixtures/phoenix-discovery/lib/bmscl_phoenix_fixture_web/controllers.ex
@@ -0,0 +1,11 @@
+defmodule BmsclPhoenixFixtureWeb.HealthController do
+  def show(_conn, _params), do: :ok
+end
+
+defmodule BmsclPhoenixFixtureWeb.DashboardController do
+  def index(_conn, _params), do: :ok
+end
+
+defmodule BmsclPhoenixFixtureWeb.UserController do
+  def create(_conn, _params), do: :ok
+end
